8a5a8b5e9f313a43d63f1f2e1010895ee0c80e0b
jacksonohallo <jacksonohallo@gmail.com>
2022-09-04 11:33:11 +0300

    done

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,13 +1,4 @@
-require_relative './dance_module'
-require_relative './meta_dancing_module'
-
-class Dancer
+class Dancer 
   extend FancyDance::ClassMethods
   include FancyDance::InstanceMethods
-
-  attr_accessor :name
-
-  def initialize(name)
-    @name = name
-  end
 end
