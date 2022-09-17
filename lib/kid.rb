4322c8cfb760f16637c7e73b7a9eff008e62013a
jacksonohallo <jacksonohallo@gmail.com>
2022-09-17 16:38:04 +0300

    done

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,4 +1,20 @@
+# require_relative './dance_module'
+# require_relative './meta_dancing_module'
+require_relative './fancy_dance'
 class Kid
-  extend FancyDance::ClassMethods
-  include FancyDance::InstanceMethods
-end
+	include FancyDance::InstanceMethods
+	extend FancyDance::ClassMethods
+
+	attr_accessor :name
+    def initialize(name)
+        @name = name
+    end   
+  end
+
+#   buster = Kid.new
+#   buster.jump
+#   # => "Look how high I'm jumping!"
+#   buster.take_a_bow
+#   # => "Thank you, thank you. It was a pleasure to dance for you all."
+#   Kid.metadata
+#   => "This class produces objects that love to dance."
