1d464cd4bc4c86406fe79263e9350943e4d3f1be
jacksonohallo <jacksonohallo@gmail.com>
2022-09-17 16:37:44 +0300

    commit tree a5184e327f960bef1ecb46d85058327ee128d2ba

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,4 +1,23 @@
-class Dancer 
-  extend FancyDance::ClassMethods
-  include FancyDance::InstanceMethods
-end
+# require_relative './dance_module'
+# require_relative './meta_dancing_module'
+require_relative './fancy_dance'
+
+class Dancer
+	include FancyDance::InstanceMethods
+	extend FancyDance::ClassMethods
+
+	attr_accessor :name
+
+    def initialize(name)
+        @name = name
+    end   
+  end
+
+#   angelina = Dancer.new
+# puts angelina.twirl
+# # => "I'm twirling!"
+# puts angelina.jump
+# # => "Look how high I'm jumping!"
+
+# puts Dancer.metadata
+# # => "This class produces objects that love to dance."
